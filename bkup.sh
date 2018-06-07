879aa30231c8b16174c9357c8ed4b38d9611991b
muhammad <Muhammad Amir>
2018-06-07 23:06:42 +1000

    Added new proj

--- bkup.sh
+++
@@ -1,7 +0,0 @@
-
-#mongoexport -d myporj -c bank -o ./bank.json
-mongoexport -d myproj -c bank -o bank_$(date +"%d-%m-%Y").json
-
-sleep 1
-
-mongoexport -d myproj -c expenses -o ./expenses_$(date +"%d-%m-%Y").json
